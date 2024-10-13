d9ba1b0c7cd735f990d306f25454dd1f276012b1
Samiur <rsamiur247@gmail.com>
2024-10-14 01:10:15 +0600

    last

--- js/test.js
+++ js/test.js
@@ -1,10 +1,12 @@
-// const isVerified = "";
-
-// // if (isVerified === true) {
-// //     console.log("user is verified");
-// // }
-// // else {
-// //     console.log("user is not verified")
-// // }
-// console.log(`${isVerified === true ? "user is verified" : "user is not verified"}`);
-function 
+what we have learnt from this project:
+1. load and display data initially
+2. on click load and display data
+3. complex object accessing
+4. toggle active class functionality
+5. search functionality
+6. load and display single data
+7. display data in a Modal 
+8. load and display single category data
+9. conditional rendering if array don't have any data
+10. conditional rendering:: if a value empty
+11.conditional rendering:: if a value length is zero
--- js/vedio.js
+++ js/vedio.js
@@ -28,9 +28,9 @@ fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
 .catch((error) => console.log(error));
 };
 
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
 //fetch the data
-fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
 .then((res) => res.json())
 .then((data) => displayVideos(data.videos))
 .catch((error) => console.log(error));
@@ -189,5 +189,9 @@ const displayCategories = (categories) => {
   });
 };
 
-loadCategories()
-loadVideos()
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  loadVideos(e.target.value);
+});
+loadCategories();
+loadVideos();
